ef6296e90b7143033c850f63f9f10f75d5209560
Giuseppe Vigneri <geppe1990@gmail.com>
2020-04-22 20:41:31 +0200

    Gatsby restored

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,10 +9,9 @@ const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <h1>Hi people</h1>
-    <h2 style={{color: 'purple'}}>Porca la madonna, funziona!!</h2>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
-    <div style={{ maxWidth: '300px', marginBottom: '1.45rem' }}>
+    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
     <Link to="/page-2/">Go to page 2</Link>
