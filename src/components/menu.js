3d8475595ce5fb9cd0b7a67db73148e118d1c8cb
Giuseppe Vigneri <geppe1990@gmail.com>
2020-05-07 00:11:32 +0200

    Update menu.js

--- src/components/menu.js
+++ src/components/menu.js
@@ -24,13 +24,13 @@ export default () => {
 			</div>
 
 			<div className="">
-				<Link to={`/blog/`} className="text-customRed inline-block mr-4 font-black transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110 hover:text-customRed">
+				<Link to={`/blog/`} className="text-customRed inline-block mr-4 font-black transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110">
 					Blog
 				</Link>
-				<Link to={`/about/`} className="text-customRed inline-block mr-4 font-black transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110 hover:text-customRed">
+				<Link to={`/about/`} className="text-customRed inline-block mr-4 font-black transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110">
 					About
 				</Link>
-				<Link to={`/test/`} className="text-customRed inline-block mr-4 font-black transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110 hover:text-customRed">
+				<Link to={`/test/`} className="text-customRed inline-block mr-4 font-black transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110">
 					Test
 				</Link>
 			</div>
